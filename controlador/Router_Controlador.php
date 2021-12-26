d9c6b9dddfb548b2f1d0db979fd7e7ce4bf210b2
nickmeza <nickmeza@upeu.edu.pe>
2021-12-25 19:35:10 -0500

    asd

--- controlador/Router_Controlador.php
+++ controlador/Router_Controlador.php
@@ -6,4 +6,16 @@ class Routes
     {
         include_once "./vistas/cliente/Inicio.php";
     }
+    public static function curso_detalle()
+    {
+        include_once "./vistas/cliente/Inicio.php";
+    }
+    public static function categoria()
+    {
+        include_once "./vistas/cliente/Inicio.php";
+    }
+    public static function curso_buscador()
+    {
+        include_once "./vistas/cliente/Inicio.php";
+    }
 }
