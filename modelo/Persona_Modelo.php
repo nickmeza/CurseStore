dc6fd2f788fb10469b5a1f809dc7ae1881ce006b
Your Name <you@example.com>
2021-12-26 01:01:57 -0500

    estructura modelo

---
+++ modelo/Persona_Modelo.php
@@ -0,0 +1,21 @@
+<?php 
+class Persona_Modelo{
+    public static function getAll(){
+        return $data=Database::query("select * from elemento ",array());
+    }
+    public static function getOne($id){
+        return $data=Database::queryOne("select * from elemento where idelemento=?",array($id));
+    }
+    public static function insertModulo($nombre){
+        Database::queryChange("insert into elemento(nombre,estado) values(?,?)",array($nombre,1));
+    }
+    public static function updateModulo($nombre,$id){
+        Database::queryChange("update elemento set nombre=? where idelemento=?",array($nombre,$id));
+    }
+    public static function delete($id){
+        Database::queryChange("delete from elemento  where idelemento=?",array($id));
+    }
+
+}
+
+?>
