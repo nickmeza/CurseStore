a6805ecab484575cf8ad2cffd54ad01be814218f
MichelleMJ02 <michellematos741@gmail.com>
2021-12-27 16:49:42 -0500

    rutas

--- controlador/RouterAdmin_Controlador.php
+++ controlador/RouterAdmin_Controlador.php
@@ -11,54 +11,47 @@ class RoutesAdmin
         if (isset($codigocurso[3]) && strlen($codigocurso[3])>0 ) {
             var_dump($codigocurso[3]);
             echo "detalle";
-            include_once "./vistas/cliente/curso/Categoria.php";
+            include_once "./vistas/administrador/cursos/DetalleCurso.php";
         }else {
             echo "curso list";
-            include_once "./vistas/cliente/curso/Categoria.php";
+            include_once "./vistas/administrador/cursos/Curso.php";
         }
     }
     public static function ventas()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/ventas/Orden.php";
     }
     public static function promocion()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/ventas/Promocion.php";
     }
     public static function descuento()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/ventas/Descuento.php";
     }
     public static function perfil()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/perfil/Cuenta.php";
     }
     public static function categoria()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/cursos/Curso.php";
     }
-    public static function buscar($buscador)
-    {
-        include_once "./vistas/cliente/Inicio.php";
-        var_dump($buscador);
-    }
+    
     public static function admin()
     {
-        include_once "./vistas/cliente/views/Inicio.php";
+        include_once "./vistas/administrador/Admin.php";
         echo "admin";
     }
     public static function profesor()
     {
         echo "profe";
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/usuario/Profesor.php";
     }
     public static function cliente()
     {
         echo "client";
-        include_once "./vistas/cliente/curso/Categoria.php";
+        include_once "./vistas/administrador/usuario/Cliente.php";
     }
-    public static function modulo()
-    {
-        include_once "./vistas/cliente/curso/Categoria.php";
-    }
+    
 }
