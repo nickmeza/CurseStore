3bfc54233152b251694575aba5ce930b55e566d6
nickmeza <nickmeza@upeu.edu.pe>
2022-01-01 21:09:30 -0500

    html para la tienda

---
+++ vistas/cliente/curso/Buscador.php
@@ -0,0 +1,43 @@
+<?php
+include_once "./vistas/cliente/component/header.php";
+?>
+<div class="conteiner">
+    <h1>Buscando <?php echo "$buscador" ?></h1>
+    <section class="container-products">
+        <div class="product">
+            <img src="https://storage.googleapis.com/site.esss.co/fc960fa4-share-cursos-online-esp.jpg" alt="" class="product_img">
+            <div class="product__description">
+                <h3 class="product_title">Nombred el curso</h3>
+                <span class="producto__price">S/.50</span>
+            </div>
+            <i class="fa fa-cart-plus"></i>
+        </div>
+        <div class="product">
+            <img src="https://storage.googleapis.com/site.esss.co/fc960fa4-share-cursos-online-esp.jpg" alt="" class="product_img">
+            <div class="product__description">
+                <h3 class="product_title"></h3>
+                <span class="producto__price"></span>
+            </div>
+            <i class="fa fa-cart-plus"></i>
+        </div>
+        <div class="product">
+            <img src="https://storage.googleapis.com/site.esss.co/fc960fa4-share-cursos-online-esp.jpg" alt="" class="product_img">
+            <div class="product__description">
+                <h3 class="product_title"></h3>
+                <span class="producto__price"></span>
+            </div>
+            <i class="fa fa-cart-plus"></i>
+        </div>
+        <div class="product">
+            <img src="https://storage.googleapis.com/site.esss.co/fc960fa4-share-cursos-online-esp.jpg" alt="" class="product_img">
+            <div class="product__description">
+                <h3 class="product_title"></h3>
+                <span class="producto__price"></span>
+            </div>
+            <i class="fa fa-cart-plus"></i>
+        </div>
+    </section>
+</div>
+<?php
+include_once "./vistas/cliente/component/footer.php";
+?>
