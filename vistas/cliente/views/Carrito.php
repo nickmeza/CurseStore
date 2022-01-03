13f5683eeb966e5442d00d37d2d2cbb4b560f923
nickmeza <nickmeza@upeu.edu.pe>
2022-01-03 16:34:37 -0500

    nosotros y carrito

---
+++ vistas/cliente/views/Carrito.php
@@ -0,0 +1,60 @@
+<?php
+include_once "./vistas/cliente/component/header.php";
+?>
+<div class="container__carrito">
+    <h1>Cesta</h1>
+    <div class="carrito__compras">
+        <div class="carrito__derecha">
+            <div>
+                <div>
+                    <div class="titulo__carrito">
+                        <h3>N cursos en la cesta</h3>
+                    </div>
+                    <div class="content__carrito">
+                        <div class="card__carrito">
+                            <div class="datelle__curso">
+                                <div class="imagen__curso">
+
+                                </div>
+                                <div class="descripcion__curso">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="titulo__carrito">
+                        <h3>N cursos en la cesta</h3>
+                    </div>
+                    <div class="content__carrito">
+                        <div class="card__carrito">
+                            <div class="datelle__curso">
+                                <div class="imagen__curso">
+
+                                </div>
+                                <div class="descripcion__curso">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="carrito__izquierda">
+            <div>
+                <div class="titulo__carrito">
+
+                </div>
+                <div class="pago__carrito">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include_once "./vistas/cliente/component/footer.php";
+?>
--- vistas/cliente/views/Nosotros.php
+++ vistas/cliente/views/Nosotros.php
@@ -1,7 +1,53 @@
 <?php
 include_once "./vistas/cliente/component/header.php";
+$empresa = array();
 ?>
 
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+<div class="contenedor">
+    <h1>contacto</h1>
+    <div class="contenido">
+        <div class="info">
+            <div class="col">
+                <i class="icono fa fa-map-marker"></i>
+                <p><?php if (isset($empresa['ubicacion'])) {
+                        echo $empresa['ubicacion'];
+                    } else {
+                        echo "ubicacion de la empresa";
+                    } ?></p>
+            </div>
+            <div class="col">
+                <i class="icono fa fa-envelope"></i>
+                <p><?php if (isset($empresa['correo'])) {
+                        echo $empresa['correo'];
+                    } else {
+                        echo "correo de la empresa";
+                    } ?></p>
+            </div>
+            <div class="col">
+                <i class="icono fa fa-phone"></i>
+                <p><?php if (isset($empresa['telefono'])) {
+                        echo $empresa['telefono'];
+                    } else {
+                        echo "telefono de la empresa";
+                    } ?></p>
+            </div>
+            <div class="redes-s">
+                <a href="https://www.facebook.com/multispaceperu"><i class="fa fa-facebook"></i></a>
+                <a href="https://www.instagram.com/3dmultispaceperu/"><i class="fa fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/company/multispace-perú/?viewAsMember=true"><i class="fa fa-linkedin"></i></a>
+            </div>
+        </div>
+        <form id="form_agregar" class="formulario">
+            <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+            <input type="text" name="correo" id="correo" placeholder="Correo">
+            <input type="text" name="asunto" id="asunto" placeholder="Asunto">
+            <textarea name="descripcion" id="descripcion" placeholder="Descripcion"></textarea>
+            <button type="submit" class="btn btn-contact btn-green"><span>ENVIAR</span></button>
+        </form>
+    </div>
+</div>
 <?php
 include_once "./vistas/cliente/component/footer.php";
 ?>
