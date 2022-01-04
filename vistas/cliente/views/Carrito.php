77d25f11bf0ef364ad75a99e89a3385f3d5e8baa
nickmeza <nickmeza@upeu.edu.pe>
2022-01-03 20:57:36 -0500

    mauqetacion del carrito de compras

--- vistas/cliente/views/Carrito.php
+++ vistas/cliente/views/Carrito.php
@@ -1,7 +1,7 @@
 <?php
 include_once "./vistas/cliente/component/header.php";
 ?>
-<div class="container__carrito">
+<div class="container">
     <h1>Cesta</h1>
     <div class="carrito__compras">
         <div class="carrito__derecha">
@@ -14,11 +14,32 @@ include_once "./vistas/cliente/component/header.php";
                         <div class="card__carrito">
                             <div class="datelle__curso">
                                 <div class="imagen__curso">
-
+                                    <img width="100%" height="100%" src="https://www.segg.es/media/noticias/344/cursos.jpg">
                                 </div>
                                 <div class="descripcion__curso">
-
+                                    <div class="datos__curso">
+                                        <p class="p__carrito p__name">Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL</p>
+                                        <p class="p__carrito">Por Juan Pablo De la torre Valdez, Creador de Código Con Juan - Aprende Con Proyectos Reales</p>
+                                    </div>
+                                    <div class="price__detail">
+                                        <p class="p__carrito p__precio">49,90 S/</p>
+                                        <p class="p__carrito">329,90 S/</p>
+                                    </div>
                                 </div>
+                                <div class="precios__curso">
+                                    <p class="p__carrito p__precio">49,90 S/</p>
+                                    <p class="p__carrito">329,90 S/</p>
+                                </div>
+                                <div class="opciones__curso">
+                                    <div class='box-left'>
+                                        <i class='fa fa-times'></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <p class="p__carrito">estrellas</p>
+                                <p class="p__carrito">clases - modulos - nivel</p>
+                                <p class="p__carrito">opciones</p>
                             </div>
                         </div>
                     </div>
@@ -31,11 +52,32 @@ include_once "./vistas/cliente/component/header.php";
                         <div class="card__carrito">
                             <div class="datelle__curso">
                                 <div class="imagen__curso">
-
+                                    <img width="100%" height="100%" src="https://www.segg.es/media/noticias/344/cursos.jpg">
                                 </div>
                                 <div class="descripcion__curso">
+                                    <div class="datos__curso">
+                                        <p class="p__carrito p__name">Desarrollo Web Completo con HTML5, CSS3, JS AJAX PHP y MySQL</p>
+                                        <p class="p__carrito">Por Juan Pablo De la torre Valdez, Creador de Código Con Juan - Aprende Con Proyectos Reales</p>
+                                    </div>
+                                    <div class="price__detail">
+                                        <p class="p__carrito p__precio">49,90 S/</p>
+                                        <p class="p__carrito">329,90 S/</p>
+                                    </div>
+                                </div>
+                                <div class="precios__curso">
+                                    <p class="p__carrito p__precio">49,90 S/</p>
+                                    <p class="p__carrito">329,90 S/</p>
+                                </div>
+                                <div class="opciones__curso">
+                                    <a href="">Eliminar</a>
+                                    <a href="">Guardar</a>
 
                                 </div>
+                            </div>
+                            <div>
+                                <p class="p__carrito">estrellas</p>
+                                <p class="p__carrito">clases - modulos - nivel</p>
+                                <p class="p__carrito">opciones</p>
                             </div>
                         </div>
                     </div>
@@ -46,10 +88,16 @@ include_once "./vistas/cliente/component/header.php";
         <div class="carrito__izquierda">
             <div>
                 <div class="titulo__carrito">
-
+                    <h3>Costos</h3>
                 </div>
                 <div class="pago__carrito">
-
+                    <div class="total__carrito">
+                        <h1>S/49,90</h1>
+                    </div>
+                    <p class="p__carrito p__separate"><b>S/329,90</b>Sub Total</p>
+                    <p class="p__carrito p__separate"><b>S/280</b>Descuento</p>
+                    <!-- HTML !-->
+                    <button class="button-48" role="button"><span class="text">Comprar</span></button>
                 </div>
             </div>
         </div>
