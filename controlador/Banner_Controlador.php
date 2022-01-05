f8fac9be4f3637881241506241c3b84cbfe4e382
nickmeza <nickmeza@upeu.edu.pe>
2022-01-05 00:00:07 -0500

    categoria CRUD al 100%

--- controlador/Banner_Controlador.php
+++ controlador/Banner_Controlador.php
@@ -2,20 +2,53 @@
 
 class Banner
 {
-    public static function get_all(){
+    public static function get_all()
+    {
         $banner = Banner_Modelo::getAll();
         echo json_encode($banner);
     }
+    public static function getById($id)
+    {
+        if (isset($id) && isset($id[4]) && strlen($id[4]) > 0) {
+            $banners = array();
+            $banners = Banner_Modelo::getOne($id[4]);
+            echo json_encode($banners);
+        } else {
+            echo "fallo al buscar";
+        }
+    }
     public static function create()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        if (isset($_REQUEST['BANNER_IMAGEN'])) {
+            $banner["BANNER_IMAGEN"] = $_REQUEST['BANNER_IMAGEN'];
+            Banner_Modelo::insertBanner($banner);
+            echo "creado correctamente";
+        } else {
+            echo "fallo al crear";
+        }
     }
-    public static function delete()
+    public static function delete($id)
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        if (isset($id) && isset($id[4]) && strlen($id[4]) > 0) {
+            $borrado = Banner_Modelo::delete($id[4]);
+            $mensaje["mensaje"] = "correcto";
+            $mensaje["statud"] = "200";
+            echo json_encode($mensaje);
+        } else {
+            echo "fallo al borrar";
+        }
     }
-    public static function update()
+    public static function update($id)
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        if (isset($_REQUEST['BANNER_IMAGEN']) && isset($_REQUEST['BANNER_STATUS'])) {
+            $banner["BANNER_IMAGEN"] = $_REQUEST['BANNER_IMAGEN'];
+            $banner["BANNER_STATUS"] = $_REQUEST['BANNER_STATUS'];
+            $mensaje["mensaje"] = "correcto";
+            $mensaje["statud"] = "200";
+            Banner_Modelo::updateBanner($banner, $id[4]);
+            echo json_encode($mensaje);
+        } else {
+            echo "fallo al actualizar";
+        }
     }
 }
--- controlador/Categoria_Controlador.php
+++ controlador/Categoria_Controlador.php
@@ -6,19 +6,55 @@ class Categoria
     {
         $categorias = array();
         $categorias = Categoria_Modelo::getAll();
-        array_splice($categorias,0,1);
+        array_splice($categorias, 0, 1);
         echo json_encode($categorias);
+    }
+    public static function getById($id)
+    {
+        if (isset($id) && isset($id[4]) && strlen($id[4]) > 0) {
+            $categorias = array();
+            $categorias = Categoria_Modelo::getOne($id[4]);
+            echo json_encode($categorias);
+        } else {
+            echo "fallo al buscar";
+        }
     }
     public static function create()
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        if (isset($_REQUEST['CAT_NAME']) && isset($_REQUEST['CAT_DESCRIPCION']) && isset($_REQUEST['CAT_IMAGEN'])) {
+            $categoria["CAT_NAME"] = $_REQUEST['CAT_NAME'];
+            $categoria["CAT_DESCRIPCION"] = $_REQUEST['CAT_DESCRIPCION'];
+            $categoria["CAT_IMAGEN"] = $_REQUEST['CAT_IMAGEN'];
+            Categoria_Modelo::insertCategoria($categoria);
+            echo "creado correctamente";
+        } else {
+            echo "fallo al crear";
+        }
     }
-    public static function delete()
+    public static function delete($id)
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        if (isset($id) && isset($id[4]) && strlen($id[4]) > 0) {
+            $borrado = Categoria_Modelo::delete($id[4]);
+            $mensaje["mensaje"] = "correcto";
+            $mensaje["statud"] = "200";
+            echo json_encode($mensaje);
+        } else {
+            echo "fallo al borrar";
+        }
     }
-    public static function update()
+    public static function update($id)
     {
-        include_once "./vistas/cliente/curso/Categoria.php";
+        if (isset($_REQUEST['CAT_NAME']) && isset($_REQUEST['CAT_DESCRIPCION']) && isset($_REQUEST['CAT_IMAGEN'])) {
+            $categoria["CAT_NAME"] = $_REQUEST['CAT_NAME'];
+            $categoria["CAT_DESCRIPCION"] = $_REQUEST['CAT_DESCRIPCION'];
+            $categoria["CAT_IMAGEN"] = $_REQUEST['CAT_IMAGEN'];
+            $categoria["CAT_ESTADO"] = $_REQUEST['CAT_ESTADO'];
+            $mensaje["mensaje"] = "correcto";
+            $mensaje["statud"] = "200";
+            Categoria_Modelo::updateCategoria($categoria, $id[4]);
+            echo json_encode($mensaje);
+        } else {
+            echo "fallo al actualizar";
+        }
     }
 }
